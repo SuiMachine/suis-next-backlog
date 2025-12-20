0c7e9d480b42019b91a9bec8cd182f57dfcc6236
Sui / SuicideMachine <SuiMachine@users.noreply.github.com>
2025-12-20 23:42:48 +0100

    Null

--- app/actions.ts
+++ app/actions.ts
@@ -188,6 +188,7 @@ export async function addNewGameAction(formData: FormData) {
                 releaseYear,
                 igdbUrl: fetchedGame.url,
                 finishedDate: null,
+                approximateDate: false,
                 comment: "",
                 timeSpent: null,
                 finished: "",
